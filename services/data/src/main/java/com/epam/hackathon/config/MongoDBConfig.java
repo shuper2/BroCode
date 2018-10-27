65390a074aa97e3f31fb2d07bbcb7c5812471135
Sanjeev Bharti <sanjeev.bharti.123@gmail.com>
2018-10-27 23:44:17 +0800

    Fetching DB details from property file

--- services/data/src/main/java/com/epam/hackathon/config/MongoDBConfig.java
+++ services/data/src/main/java/com/epam/hackathon/config/MongoDBConfig.java
@@ -4,20 +4,27 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MongoDBConfig {
+
+    @Value("${mongo.db.uri}")
+    private String mongoDbUri;
+    @Value("${mongo.db.database}")
+    private String mongoDatabase;
 
     @Bean(name = "dataSource")
     public Datastore getDataSource() {
-        MongoClientURI uri = new MongoClientURI(
-                "mongodb+srv://testuser:testpassword@brocode-i63ag.mongodb.net");
+        MongoClientURI uri = new MongoClientURI(mongoDbUri);
 
         MongoClient mongoClient = new MongoClient(uri);
         Morphia morphia = new Morphia();
-        Datastore dataStore = morphia.createDatastore(mongoClient, "MyDataBase");
+        Datastore dataStore = morphia.createDatastore(mongoClient, mongoDatabase);
         return dataStore;
     }
 }

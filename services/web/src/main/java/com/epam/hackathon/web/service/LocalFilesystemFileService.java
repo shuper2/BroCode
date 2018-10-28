3f8abf57837964f3c63790bc178d3a5f5757ca06
Sanjeev Bharti <sanjeev.bharti.123@gmail.com>
2018-10-28 21:47:19 +0800

    Set file path and image location

--- services/web/src/main/java/com/epam/hackathon/web/service/LocalFilesystemFileService.java
+++ services/web/src/main/java/com/epam/hackathon/web/service/LocalFilesystemFileService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -22,8 +23,8 @@ class LocalFilesystemFileService implements FileService {
     @Override
     public String save(final MultipartFile file, final ImageType imageType) throws IOException {
         String fileExtension = FilenameUtils.getExtension(file.getOriginalFilename());
-        String filename = imageType + "_" + System.currentTimeMillis() + "." + fileExtension;
-        Files.write(Paths.get(localStorageDir).resolve(filename), file.getBytes());
+        String filename = System.currentTimeMillis() + "." + fileExtension;
+        Files.write(Paths.get(localStorageDir + System.getProperty("file.separator") + imageType).resolve(filename), file.getBytes());
         return filename;
     }
 }

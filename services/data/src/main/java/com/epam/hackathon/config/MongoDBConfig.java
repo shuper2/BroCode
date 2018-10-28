e8ab7dbbcb89c8b2bc3d262a326cf163612bfb13
Cause Chung <cuzfrog@139.com>
2018-10-28 19:26:41 +0800

    resolve properties conflicts

--- services/data/src/main/java/com/epam/hackathon/config/MongoDBConfig.java
+++ services/data/src/main/java/com/epam/hackathon/config/MongoDBConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
+@PropertySource("classpath:data.properties")
 public class MongoDBConfig {
 
     @Value("${mongo.db.uri}")
